14adf3c9b5c67ea92c2da65804d1f6f5d7aa8eb6
tete0611 <tete0600@naver.com>
2023-04-13 14:52:41 +0900

    welcome 스티키 기능 전달

--- events/messageCreate/welcome.js
+++ events/messageCreate/welcome.js
@@ -18,26 +18,17 @@ module.exports = {
   async execute(message) {
     if (message.author.bot) return;
     if (message.channelId === welcomeChannelId) {
-      const { channel } = message;
-      const messages = await channel.messages.fetch({ limit: 5 });
-      const botMessages = messages.filter(msg => msg.author.id === process.env.ID);
-      const messagesToDelete = botMessages.filter(msg => msg.id !== message.id).map(msg => msg.id);
-      if (messagesToDelete.length > 0) channel.bulkDelete(messagesToDelete);
-      await channel.send({
-        content: `Hello! New villiger!👋🏻\nWelcome to mozips village!🏡\nMozips Village is a place where you can learn and practice Korean together.\nShall we greet our Mozips Villigers?💬\n1️⃣ Please let us know your name (nickname).\n2️⃣ How long have you been learning Korean?\n3️⃣ Which country are you from?\n4️⃣ Say a word to our villigers`,
-      });
-
       if (!checkRole(message.member, 'Manager')) {
         messageCount += 1;
         if (messageCount === 20) {
-          message.channel.send({
+          await message.channel.send({
             content: `모두 환영합니다! <#1051667735773458542> 채널에서 채팅을 시작해보세요.\nWelcome to Mozips village!\nThis channel is to introduce yourself.\nPlease use <#1051667735773458542> channel to chat :cherries:\nAnd don't forget to get your roles from <#1051689953295351818>`,
           });
         }
         if (message.content.includes('안녕')) {
-          message.reply({ content: `**반갑습니다!** :slight_smile:` });
+          await message.reply({ content: `**반갑습니다!** :slight_smile:` });
         } else if (REG_EXP.hello.test(message.content) || REG_EXP.hi.test(message.content)) {
-          message.reply({ content: `**Hello!** :slight_smile:` });
+          await message.reply({ content: `**Hello!** :slight_smile:` });
         }
       }
     } else if (
