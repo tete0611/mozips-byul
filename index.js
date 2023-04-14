42648b876ce08179f1fb13c95c9ea14be82c393a
tete0611 <tete0600@naver.com>
2023-04-14 17:28:38 +0900

    생일 기능 추가

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { Client, Collection, REST, Routes, GatewayIntentBits } = require('discord.js');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const { env } = process;
 
 /** 서버연결 */
-// mongoose
-//   .connect(env.END_POINT, { dbName: env.DB_NAME })
-//   .then(console.log('데이터베이스 연결완료'))
-//   .catch(console.error);
+mongoose
+  .connect(env.END_POINT, { dbName: env.DB_NAME })
+  .then(console.log('데이터베이스 연결완료'))
+  .catch(console.error);
 
 /** 클라이언트로 부터 수신할 패킷 선언 */
 const client = (module.exports = new Client({
